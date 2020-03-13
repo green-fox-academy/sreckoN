686380cf1fc0b7862df99708241ba736bbe2bece
sreckoN <srecko.nikolic@protonmail.com>
2020-03-13 14:46:53 +0100

    Josephus problem refactored

--- week-02/day-02/src/oldExcercises/JosephusProblem.java
+++ week-02/day-02/src/oldExcercises/JosephusProblem.java
@@ -31,32 +31,40 @@ public class JosephusProblem {
             }
         }
 
+        // Returns the position of the person that survived
         return people.get(0);
     }
 
     private static ArrayList<Integer> makeList(int x) {
         ArrayList<Integer> list = new ArrayList<>();
+
+        // Each element in the list is the position of a person in a circle
         for (int i = 1; i <= x; i++) {
             list.add(i);
         }
+
         return list;
     }
 
     private static int kill(ArrayList<Integer> people, int whoKills) {
         int toKill = -1;
-        if (whoKills == 0) {
-            for (int i = 0; i < people.size(); i++) {
-                if (i % 2 != 0) people.set(i, -1);
-            }
-        } else {
-            for (int i = 0; i < people.size(); i++) {
-                if (i % 2 == 0) people.set(i, -1);
-            }
+        boolean killsTheFirstOne = (whoKills == 0) ? true : false;
+
+        // Starts the killing round
+        for (int i = 0; i < people.size(); i++) {
+                if (killsTheFirstOne) {
+                    if (i % 2 != 0) people.set(i, -1);
+                } else {
+                    if (i % 2 == 0) people.set(i, -1);
+                }
         }
 
+        // If the last person is dead, then the first person in list starts the killing in the next round
+        // If not then the last person in the list starts the killing
         if (people.get(people.size() - 1) == -1) toKill = 0;
         else toKill = people.size() - 1;
 
+        // Removes the dead people from the list
         for (int i = 0; i < people.size(); i++) {
             if (people.get(i) == -1) people.remove(i);
         }
