6585d0c748daf83966be3c6633ed7b2955e49f90
sreckoN <srecko.nikolic@protonmail.com>
2020-04-16 14:55:01 +0200

    HelloWorldREST done

---
+++ week-06/day-03/helloWorld/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import com.greenfoxacademy.springstart.domain.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRESTController {
+
+  @RequestMapping(value = "/greeting")
+  public Greeting greeting() {
+    return new Greeting(1, "Hello World!");
+  }
+}
