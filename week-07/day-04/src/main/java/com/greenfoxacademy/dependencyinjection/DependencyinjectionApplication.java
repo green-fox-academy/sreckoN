34709d766d20f475bfe2a9f08da1c96607838704
sreckoN <srecko.nikolic@protonmail.com>
2020-04-24 12:10:41 +0200

    Folder for the DI exercises

---
+++ week-07/day-04/src/main/java/com/greenfoxacademy/dependencyinjection/DependencyinjectionApplication.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.dependencyinjection;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyinjectionApplication implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DependencyinjectionApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    
+  }
+}
