ed876c9354b5106836f7bb6848b0472072371eca
sreckoN <srecko.nikolic@protonmail.com>
2020-04-26 18:26:53 +0200

    ProgrammerClub project created

---
+++ week-07/day-05/src/test/java/com/greenfox/programmerclub/ProgrammerclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.programmerclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerclubApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
